2faa419a20f09e1ba685b7b9e96c6adae9a6014d
Lucian Marin <marin.lucian@gmail.com>
2016-07-23 07:23:31 +0300

    working amp image size

--- index.js
+++ index.js
@@ -2,25 +2,63 @@
 
 var program = require('commander');
 var readability = require('node-readability');
-var htmlToAmp = require('html-to-amp');
+
+var htmlToArticleJson = require('html-to-article-json')({});
+var convertToAmp = require('article-json-to-amp');
+
+var Promise = require('bluebird');
+var size = require('request-image-size');
 
 program
-    .version('1.1.7')
+    .version('1.2.0')
     .option('-u, --url [type]', 'Input article URL')
     .option('-h, --html [type]', 'Input HTML code')
     .parse(process.argv);
 
 if (program.url && !program.html) {
     readability(program.url, function(err, article, meta) {
-        htmlToAmp(article.content).then(function (amp) {
-            console.log(amp);
-        });
+        amped(article.content);
         article.close();
     });
 }
 
 if (program.html && !program.url) {
-    htmlToAmp(program.html).then(function (amp) {
-        console.log(amp);
+    amped(program.html)
+}
+
+function amped(html) {
+    var article = htmlToArticleJson(html);
+    var promises = Promise.all(article.map(function(row) {
+        var embedType = row.embedType;
+        var width = row.width;
+        var height = row.height;
+        var src = row.src;
+
+        if (embedType !== 'image' || width && height || !src) {
+            return row;
+        }
+
+        var options = {
+            url: src,
+            timeout: 1000,
+            headers: {
+                'User-Agent': 'request-image-size'
+            }
+        };
+
+        return new Promise(function (resolve, reject) {
+            size(options, function (err, dimensions) {
+                if (err) {
+                    reject(err);
+                } else {
+                    row.width = dimensions.width;
+                    row.height = dimensions.height;
+                    resolve(row);
+                }
+            });
+        });
+    }));
+    promises.then(function() {
+        console.log(convertToAmp(article));
     });
 }
